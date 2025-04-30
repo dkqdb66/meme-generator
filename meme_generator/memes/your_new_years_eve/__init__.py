de9647b0c047e026048ac66e8f4a99135ff5bcef
電撃メロン <109346923+dkqdb66@users.noreply.github.com>
2025-04-30 18:50:36 +0800

    Update __init__.py

--- meme_generator/memes/your_new_years_eve/__init__.py
+++ meme_generator/memes/your_new_years_eve/__init__.py
@@ -1,34 +1,63 @@
 from datetime import datetime
 from pathlib import Path
+from typing import Literal
 
 from pil_utils import BuildImage
+from pydantic import Field
 
-from meme_generator import add_meme
-from meme_generator.utils import make_jpg_or_gif
+from meme_generator import (
+    MemeArgsModel,
+    MemeArgsType,
+    ParserArg,
+    ParserOption,
+    add_meme,
+    CommandShortcut,
+)
 
 img_dir = Path(__file__).parent / "images"
 
+help_text = "假日编号：1、跨年，2、五一"
 
-def your_new_years_eve(images: list[BuildImage], texts: list[str], args):
-    frame = BuildImage.open(img_dir / "0.png")
+class Model(MemeArgsModel):
+    holiday:int = Field(0,description=help_text)
 
-    def make(imgs: list[BuildImage]) -> BuildImage:
-        img = (
-            imgs[0]
-            .convert("RGBA")
-            .resize((586, 430), inside=True, keep_ratio=True, bg_color="white")
+args_type = MemeArgsType(
+    args_model=Model,
+    args_examples=[Model(holiday=h) for h in range(2)],
+    parser_options=[
+        ParserOption(
+            names=["-h", "--holiday","假日编号"],
+            args=[ParserArg(name="holiday", value="str")],
+            help_text=help_text,
         )
-        return frame.copy().paste(img, (0, 650), alpha=True, below=True)
+    ],
+)
 
-    return make_jpg_or_gif(images, make)
+
+def your_holiday(images: list[BuildImage], texts: list[str], args:Model):
+    frame = BuildImage.open(img_dir / f"{args.holiday - 1}.png")
+    img = images[0].convert("RGBA").resize((586,430),inside=True,keep_ratio=True)
+    frame.paste(img,(0,650),alpha=True,below=True)
+    return frame.save_jpg()
 
 
 add_meme(
-    "your_new_years_eve",
-    your_new_years_eve,
+    "your_holiday",
+    your_holiday,
     min_images=1,
     max_images=1,
-    keywords=["你的跨年"],
-    date_created=datetime(2024, 12, 31),
-    date_modified=datetime(2024, 12, 31),
+    args_type=args_type,
+    keywords=["你的假日"],
+    shortcuts=[
+        CommandShortcut(
+            key="你的" + k,
+            args = ["--holiday",f"{v}"]
+        )
+        for k,v in {
+            "跨年":1,
+            "五一":2,
+        }.items()
+    ],
+    date_created=datetime(2024,12,31),
+    date_modified=datetime(2024,12,31),
 )
